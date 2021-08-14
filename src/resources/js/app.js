22362a2cf809f01c2f1c869fad7926313712e02d
Pedro Pablo Pastene <pe.pastene@gmail.com>
2021-08-14 18:36:58 -0400

    Feature/inertia vue (#22)
    * Feat: Generated files from laravel-mix
    
    * Refactor: Change packages and code from Vue3 to Vue2

--- src/resources/js/app.js
+++ src/resources/js/app.js
@@ -1,16 +1,16 @@
 require('./bootstrap');
 
-import { createApp, h } from 'vue'
-import { createInertiaApp } from '@inertiajs/inertia-vue3'
+import Vue from 'vue'
+import { createInertiaApp } from '@inertiajs/inertia-vue'
 import { InertiaProgress } from '@inertiajs/progress'
 
 InertiaProgress.init()
 
 createInertiaApp({
   resolve: name => require(`./Pages/${name}`),
-  setup({ el, App, props, plugin }) {
-    createApp({ render: () => h(App, props) })
-      .use(plugin)
-      .mount(el)
+  setup({ el, App, props }) {
+    new Vue({
+      render: h => h(App, props),
+    }).$mount(el)
   },
 })
