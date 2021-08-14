24b619a8d4ecdaa3cec2da88d3f1ea5167fdc3a1
Pedro Pablo Pastene <pe.pastene@gmail.com>
2021-08-14 03:19:18 -0400

    Feat: Add aditional config into webpack.mix.js

--- src/webpack.mix.js
+++ src/webpack.mix.js
@@ -11,7 +11,12 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
+ mix
+    .js('resources/js/app.js', 'public/js')
+    .vue()
+    .alias({ '@': 'resources/js' })
     .postCss('resources/css/app.css', 'public/css', [
-        //
-    ]);
+
+    ])
+    .version()
+    .sourceMaps()
