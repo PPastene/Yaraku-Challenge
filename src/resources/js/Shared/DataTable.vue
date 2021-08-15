28228b87e4c082a36a4c604c88b5edcf91047947
Pedro Pablo Pastene <pe.pastene@gmail.com>
2021-08-15 18:52:13 -0400

    Feat: Creates new component which renders a data-table and receive the book list as a prop

---
+++ src/resources/js/Shared/DataTable.vue
@@ -0,0 +1,21 @@
+<template>
+    <v-data-table
+        :headers="headers"
+        :items="items"
+    ></v-data-table>
+</template>
+<script>
+export default {
+    props: {
+        items: Object,
+    },
+    data() {
+        return {
+            headers: [
+                { text: 'Title', value: 'title' },
+                { text: 'Author', value: 'author' },
+            ]
+        }
+    }
+}
+</script>
