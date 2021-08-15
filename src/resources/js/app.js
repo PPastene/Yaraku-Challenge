e85b021718016fdb18384823ae31100be111efed
Pedro Pablo Pastene <pe.pastene@gmail.com>
2021-08-14 22:20:05 -0400

    Fix/inertia vue (#24)
    * Refactor: Change in packages version
    
    * Fix: Small changes in javascript load

--- src/resources/js/app.js
+++ src/resources/js/app.js
@@ -1,16 +1,16 @@
-require('./bootstrap');
-
 import Vue from 'vue'
 import { createInertiaApp } from '@inertiajs/inertia-vue'
 import { InertiaProgress } from '@inertiajs/progress'
 
+Vue.config.productionTip = false
+
 InertiaProgress.init()
 
 createInertiaApp({
-  resolve: name => require(`./Pages/${name}`),
-  setup({ el, App, props }) {
+  resolve: name => require(`./Pages/${name}.vue`),
+  setup({ el, app, props }) {
     new Vue({
-      render: h => h(App, props),
+      render: h => h(app, props),
     }).$mount(el)
   },
 })
