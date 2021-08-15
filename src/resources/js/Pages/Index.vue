9ab6e0731043d3831206c574c399974ca0cb4ba9
Pedro Pablo Pastene <pe.pastene@gmail.com>
2021-08-15 18:54:29 -0400

    Feature/frontend book list (#31)
    * Feat: Include the book list into the Inertia render
    
    * Feat: Creates new component which renders a data-table and receive the book list as a prop
    
    * Feat: Import DataTable component, call it into the template and insert the book list as a prop

--- src/resources/js/Pages/Index.vue
+++ src/resources/js/Pages/Index.vue
@@ -2,14 +2,27 @@
     <layout>
         <v-container>
             <h1>Here goes the beauty of this website</h1>
+            <data-table
+                :items="books"
+            ></data-table>
         </v-container>
     </layout>
 </template>
 <script>
 import Layout from '@/Shared/Layout'
+import DataTable from '@/Shared/DataTable'
 export default {
     components: {
-        Layout
+        Layout,
+        DataTable
+    },
+    props: {
+        books: Object
+    },
+    data() {
+        return {
+
+        }
     }
 }
 </script>
