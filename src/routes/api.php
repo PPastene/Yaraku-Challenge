05061d5a2c6318d0fbb0b99cfecae567d39370b6
Pedro Pablo Pastene <pe.pastene@gmail.com>
2021-08-19 05:30:02 -0400

    Feat: Remove unused route on api.php

--- src/routes/api.php
+++ src/routes/api.php
@@ -18,5 +18,3 @@ use App\Http\Controllers\HomeController;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
-
-Route::post('/books/search', [HomeController::class, 'search']);
